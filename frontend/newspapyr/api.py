11bc0a382558cb0341db07229669993dd928013d
Fox Wilson <foxwilson123@gmail.com>
2015-09-19 07:55:29 -0400

    api

--- frontend/newspapyr/api.py
+++ frontend/newspapyr/api.py
@@ -1,2 +1,12 @@
-from requests import get
+from requests import post
+from newspapyr.config import config
 
+class ApiWrapper:
+    def __init__(self, path="/"):
+        self.path = path
+
+    def __getitem__(self, x):
+        return ApiWrapper(self.path + x + "/")
+
+    def __call__(self, **kwargs):
+        return post(self.path, data=kwargs, cert=(config["ssl"]["crt"], config["ssl"]["key"])).json()
