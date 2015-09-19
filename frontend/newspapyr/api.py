575cf281c168beb791bf5117af662a4b1588216f
Fox Wilson <foxwilson123@gmail.com>
2015-09-19 07:59:22 -0400

    ssl verify

--- frontend/newspapyr/api.py
+++ frontend/newspapyr/api.py
@@ -1,12 +1,17 @@
 from requests import post
+from newspapyr import app
 from newspapyr.config import config
 
 class ApiWrapper:
-    def __init__(self, path="/"):
+    def __init__(self, path="https://service1.newspapyr.co/"):
         self.path = path
 
     def __getitem__(self, x):
         return ApiWrapper(self.path + x + "/")
 
     def __call__(self, **kwargs):
-        return post(self.path, data=kwargs, cert=(config["ssl"]["crt"], config["ssl"]["key"])).json()
+        return post(self.path, data=kwargs, cert=(config["ssl"]["crt"], config["ssl"]["key"]), verify=config["ssl"]["ca"]).json()
+
+@app.context_processor
+def inject():
+    return dict(api=ApiWrapper())
