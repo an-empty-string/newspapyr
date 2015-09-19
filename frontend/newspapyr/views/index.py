70cbcb476757ffd0a8b0e5ad33bd9adeb8be12b9
Srijay Kasturi <srijay@techfilmer.com>
2015-09-19 09:52:09 -0400

    dankest registration system ever!!111!!!!one!!!

--- frontend/newspapyr/views/index.py
+++ frontend/newspapyr/views/index.py
@@ -1,6 +1,24 @@
-from flask import render_template
+from flask import render_template, session, request
 from newspapyr import app
+from newspapyr.api import ApiWrapper
+
+api = ApiWrapper()
+
+app.secret_key = 'DANK MEMES'
+
+def session(uid):
+	session['uid'] = uid
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/register', methods = ['POST']):
+def register():
+	i = request.form.to_dict()
+	uid = api.auth.register(username=i["username"], password=i["password"])
+	session(uid)
+	return "whoooooo, you're registered with uid" + uid
+
+@app.route('/register')
+	return render_template()
