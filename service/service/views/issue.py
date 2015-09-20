7191e28cb3b2529ff8734965a174c7b97b040766
TCYRUS <tcyrus@users.noreply.github.com>
2015-09-20 02:35:06 -0400

    Added Create Issue Code

--- service/service/views/issue.py
+++ service/service/views/issue.py
@@ -9,6 +9,14 @@ def recent_issues(args):
 def all_issues(args):
     return dict(issues=list(Issue.select()))
 
+@method("issue.create")
+def create_issue(args):
+    Title = args["title"]
+    Source = args["source"]
+    Time = args["time"]
+    Text = args["text"]
+    return dict(uid=Issue.create(title = Title, source = Source, time = Time, text = Text).id)
+
 @method("tag.for_issue")
 def issue_tags(args):
     return dict(tags=[i.word for i in Keyword.select().where(Keyword.article == Issue.get(args["aid"]))])
